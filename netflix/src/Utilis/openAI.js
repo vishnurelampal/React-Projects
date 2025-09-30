1e2d67eb45cc2911f125a20d0b932582186fe788
Vishnu R <vishnurelampal@gmial.com>
2025-10-01 01:14:43 +0530

    openai removeed

--- netflix/src/Utilis/openAI.js
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-import { GPT_KEY } from "./Constants";
-console.log(GPT_KEY);
-const client = new OpenAI({
-  apiKey: GPT_KEY,
-  dangerouslyAllowBrowser: true,
-});
-export default client;
